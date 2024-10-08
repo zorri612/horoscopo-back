1b2ce3ea2120a7a3978fbde8476c7a0b216212db
Nicolas <zorrillanico200312@gmail.com>
2024-10-08 10:03:46 -0500

    se creo el GIT

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ app.use(urlencoded({ extended: true }));
 app.use(json());
 app.use(cors());
 
+app.get('/', async (req,res) => {
+    res.send("Hola soy el back del horoscopo de Nico");
+});
+
 app.use('/v1/signos', signosRouter);
 app.use('/v1/auth', authRouter); // Utilizar las rutas de autenticación
 
